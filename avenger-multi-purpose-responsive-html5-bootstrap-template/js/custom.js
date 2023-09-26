2798e81a1627f8702944f9b386d3d5919fb013f0
HYEOKBEOM <part1shb@gmail.com>
2023-09-26 21:47:21 +0900

    custom

--- avenger-multi-purpose-responsive-html5-bootstrap-template/js/custom.js
+++ avenger-multi-purpose-responsive-html5-bootstrap-template/js/custom.js
@@ -6,7 +6,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image").backstretch('images/club.jpeg');
 		
 		// Fixed header
 		//-----------------------------------------------
